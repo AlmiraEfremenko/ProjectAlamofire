3fb343be246e99bdc79f23157ccb24d4b49ea1d9
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-02-24 16:58:28 +0500

    Initial Commit

---
+++ ProjectAlamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectAlamofire
+//
+//  Created by MAC on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
