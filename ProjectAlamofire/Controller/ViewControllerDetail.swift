79799f5200e43198536e51d2a1f266a5be7a8d02
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-02-28 18:46:11 +0500

    Add viewControllerDetail of card().

--- ProjectAlamofire/Controller/ViewControllerDetail.swift
+++ ProjectAlamofire/Controller/ViewControllerDetail.swift
@@ -5,7 +5,6 @@
 //  Created by MAC on 25.02.2022.
 //
 
-import Foundation
 import UIKit
 
 class ViewControllerDetail: UIViewController {
@@ -24,12 +23,12 @@ class ViewControllerDetail: UIViewController {
         return name
     }()
     
-        private lazy var type: UILabel = {
-            var type = UILabel()
-            type.font = .systemFont(ofSize: 15)
-            type.translatesAutoresizingMaskIntoConstraints = false
-            return type
-        }()
+    private lazy var type: UILabel = {
+        var type = UILabel()
+        type.font = .systemFont(ofSize: 15)
+        type.translatesAutoresizingMaskIntoConstraints = false
+        return type
+    }()
     
     private lazy var text: UILabel = {
         var text = UILabel()
@@ -37,7 +36,6 @@ class ViewControllerDetail: UIViewController {
         text.textAlignment = .center
         text.contentMode = .scaleToFill
         text.numberOfLines = 0
-        
         text.translatesAutoresizingMaskIntoConstraints = false
         return text
     }()
@@ -45,7 +43,6 @@ class ViewControllerDetail: UIViewController {
     private lazy var imageUrl: UIImageView = {
         var imageUrl = UIImageView()
         imageUrl.translatesAutoresizingMaskIntoConstraints = false
-        
         return imageUrl
     }()
     
@@ -55,7 +52,6 @@ class ViewControllerDetail: UIViewController {
             self.type.text = cardItems?.type
             self.text.text = cardItems?.text
             
-            
             guard let imagePath = cardItems?.imageUrl,
                   let imageUrl = URL(string: imagePath),
                   let imageData = try? Data(contentsOf: imageUrl)
@@ -63,20 +59,14 @@ class ViewControllerDetail: UIViewController {
                 self.imageUrl.image = UIImage(named: "card")
                 return
             }
-           
-                self.imageUrl.image = UIImage(data: imageData)
-            
-           
-               
+            self.imageUrl.image = UIImage(data: imageData)
         }
     }
-  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         setupHierarchy()
         setupLayout()
-        
     }
     
     private func setupHierarchy() {
@@ -85,12 +75,9 @@ class ViewControllerDetail: UIViewController {
         parentsStackView.addSubview(type)
         parentsStackView.addSubview(text)
         parentsStackView.addSubview(imageUrl)
-       
-        
     }
     
     private func setupLayout() {
-     
         parentsStackView.translatesAutoresizingMaskIntoConstraints = false
         parentsStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
         parentsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
@@ -105,10 +92,9 @@ class ViewControllerDetail: UIViewController {
         
         type.centerXAnchor.constraint(equalTo: parentsStackView.centerXAnchor).isActive = true
         type.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 30).isActive = true
-
+        
         text.centerXAnchor.constraint(equalTo: parentsStackView.centerXAnchor).isActive = true
         text.topAnchor.constraint(equalTo: type.bottomAnchor, constant: 30).isActive = true
         text.trailingAnchor.constraint(equalTo: parentsStackView.trailingAnchor, constant: -10).isActive = true
     }
-
 }
