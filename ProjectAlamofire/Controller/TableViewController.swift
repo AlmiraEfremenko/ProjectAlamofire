4ab005d3eec419cb4da7b34400edb0f6c2c25242
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-02-28 18:46:31 +0500

    Add tableViewController card().

--- ProjectAlamofire/Controller/TableViewController.swift
+++ ProjectAlamofire/Controller/TableViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
-   
+class TableViewController: UIViewController {
+    
     private var cards = [Card]()
- private var filteredCards = [Card]()
+    private var filteredCards = [Card]()
     
     var isSearchBarEmpty: Bool {
         return searchController.searchBar.text?.isEmpty ?? true
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     var isFiltering: Bool {
         return searchController.isActive && !isSearchBarEmpty
     }
-   
+    
     private lazy var tableView: UITableView = {
         var tableView = UITableView(frame: .zero, style: .plain)
         tableView.rowHeight = 150
@@ -38,10 +38,9 @@ class ViewController: UIViewController {
         searchController.searchResultsUpdater = self
         searchController.searchBar.placeholder = "Search name card..."
         definesPresentationContext = true
-        
         return searchController
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Cards"
@@ -49,9 +48,8 @@ class ViewController: UIViewController {
         setupHierarchy()
         setupLayout()
         fetchData()
-       
     }
- 
+    
     private func setupHierarchy() {
         view.addSubview(tableView)
     }
@@ -66,18 +64,27 @@ class ViewController: UIViewController {
     
     func fetchData() {
         AF.request("https://api.magicthegathering.io/v1/cards").responseDecodable(of: Cards.self) {  (data) in
-            guard let cardData = data.value else { return }
-            let cards = cardData.cards
-            self.cards = cards
-            self.tableView.reloadData()
+            switch data.result {
+            case .success:
+                if let JSON = data.value {
+                    guard let cardData = data.value else { return }
+                    let cards = cardData.cards
+                    self.cards = cards
+                    self.tableView.reloadData()
+                    print(JSON)
+                    let status = data.response?.statusCode
+                    print("Status code \(status ?? 404)")
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
     }
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
+extension TableViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         
         if isFiltering {
             return filteredCards.count
@@ -86,35 +93,27 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource, UISearchRe
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.identifier, for: indexPath) as? TableViewCell else { return UITableViewCell() }
-        
-        
-            cell.card = self.cards[indexPath.row]
-           
+        cell.card = self.cards[indexPath.row]
         cell.accessoryType = .disclosureIndicator
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
         tableView.deselectRow(at: indexPath, animated: true)
-        
         let card = self.cards[indexPath.row]
-        
         let vc = ViewControllerDetail()
         vc.cardItems = card
         self.navigationController?.pushViewController(vc, animated: true)
     }
+}
+
+extension TableViewController: UISearchResultsUpdating {
     
     func updateSearchResults(for searchController: UISearchController) {
-        
-       
         filteredCards = cards.filter({ (card: Card) -> Bool in
             return (card.name.lowercased().contains(searchController.searchBar.text?.lowercased() ?? ""))
         })
         tableView.reloadData()
     }
-    
 }
-
